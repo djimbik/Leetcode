9e70000bf038006e6f068e7231f15580acff0f73
RomanB <djimbik93@gmail.com>
2023-05-14 11:57:14 +0300

    1 easy task done - filter

--- easy lvl.js
+++ easy lvl.js
@@ -23,3 +23,22 @@ const compose = function(functions) {
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
  */
+
+// ===================================================================================================
+// easy 3 2634. Filter Elements from Array
+//Given an integer array arr and a filtering function fn, return a new array with a
+// fewer or equal number of elements.
+//
+// The returned array should only contain elements where fn(arr[i], i) evaluated to a truthy value.
+//
+// Please solve it without the built-in Array.filter method.
+
+const filter = function(arr, fn) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
